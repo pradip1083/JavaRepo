efb66ebb6e739804af676f983628f440d02c5dd6
pradip1083 <pradipnikam1083@gmail.com>
2019-10-31 12:42:57 +0530

    changing camel config file name..

--- apcheCamel/src/main/java/com/camel/apacheCamel/application/ApcheCamelApplication.java
+++ apcheCamel/src/main/java/com/camel/apacheCamel/application/ApcheCamelApplication.java
@@ -1,16 +1,7 @@
 package com.camel.apacheCamel.application;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class ApcheCamelApplication {
--- apcheCamel/src/main/java/com/camel/apacheCamel/application/controller/RestControler.java
+++ apcheCamel/src/main/java/com/camel/apacheCamel/application/controller/RestControler.java
@@ -14,15 +14,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Controller  // @Controller is used to create the Map of Model object and find the View
+				
+/*
+ *  @RestController is used to simply returns the object and object data is directly written into HTTP response
+ *  as JSON or XML 
+ */
 @RequestMapping("/api")
 public class RestControler {
 	
 	@GetMapping("/camel/{name}")
-	public String getData(@PathVariable(name="name") String f_name,Model model) throws Exception {
+	public Object getData(@PathVariable(name="name") String f_name,Model model) throws Exception {
 		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext-camel.xml");
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("camel-config.xml");
 		
 		CamelContext camelcontext = SpringCamelContext.springCamelContext(appContext, false);
 		ProducerTemplate template = camelcontext.createProducerTemplate();
@@ -41,7 +47,7 @@ public class RestControler {
 		List<String>result=template.requestBodyAndHeader("direct:broadband",map, "header", headers, List.class);
 		
 		System.out.println("The Final Result of camel output:.... "+result);
-		model.addAttribute("map_data", map);
+		model.addAttribute("map_data", result);
 		return "welcome";
 		
 	}
